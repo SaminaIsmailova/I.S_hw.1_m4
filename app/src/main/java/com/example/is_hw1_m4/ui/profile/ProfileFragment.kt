024ed4a6a25ded83414e362977decaa5ebd894f6
SaminaIsmailova <samina.ismailova.06@gmail.com>
2023-12-18 16:30:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/is_hw1_m4/ui/profile/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.example.is_hw1_m4.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.is_hw1_m4.R
+import com.example.is_hw1_m4.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "I.S_hw.1_m4"
+include(":app")
+ 
